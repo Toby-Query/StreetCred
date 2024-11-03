c794280fdcd8801c80afa7572142e7f709881c97
SOMEONE1703 <siphelel.1703@gmail.com>
2024-11-03 14:14:02 +0200

    showroom ui

--- src/showroom/main.js
+++ src/showroom/main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import details from "./details.json";
-console.log(details);
+
 // Set up Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
